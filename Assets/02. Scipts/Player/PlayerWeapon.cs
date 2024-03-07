aee4241426a826c63826321f2ccf08e8578ed575
공지수 <giroo27@gmail.com>
2024-03-07 18:23:33 +0900

    [공지수] 인벤토리 매니저 및 플레이어 무기 교체 스크립트 -> 작동 잘 되는거 확인하고 집에 감

---
+++ Assets/02. Scipts/Player/PlayerWeapon.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerWeaponState
+{
+    Sword,
+    MagicWand,
+}
+
+public class PlayerWeapon : MonoBehaviour
+{
+    public static PlayerWeapon Instance { get; private set; }
+    private PlayerWeaponState _currentState = PlayerWeaponState.Sword;
+
+    public GameObject Sword;
+    public GameObject MagicWand;
+    public Vector3 _target;
+    public GameObject inventoryUI;
+
+    void Start()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); // 선택적: 씬 전환 시 파괴되지 않도록 함
+            inventoryUI.SetActive(false);
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            Debug.Log("Tab key pressed.");
+            inventoryUI.SetActive(!inventoryUI.activeSelf);
+        }
+    }
+
+    public void WeaponSwitch(PlayerWeaponState playerWeaponState)
+    {
+        _currentState = playerWeaponState;
+        switch (_currentState) 
+        {
+            case PlayerWeaponState.Sword:
+                Sword.gameObject.SetActive(true);
+                MagicWand.gameObject.SetActive(false);
+            break;
+            case PlayerWeaponState.MagicWand:
+                MagicWand.gameObject.SetActive(true);
+                Sword.gameObject.SetActive(false);
+            break;
+        }
+        transform.position = _target;
+    }
+  
+}
