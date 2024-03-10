48f2442e5a1cb82dde1e1f6f6c0dd96dc581d7f3
badabab <ky040723@gmail.com>
2024-03-10 12:25:55 +0900

    [김예은] 잡몹 만드는 중이었는데 보스 무기 수정 부터 할거임

--- Assets/02. Scipts/Enemy/Enemy.cs
+++ Assets/02. Scipts/Enemy/Enemy.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public enum EnemyState
 {
@@ -14,7 +15,37 @@ public enum EnemyState
 }
 public class Enemy : MonoBehaviour
 {
-    private EnemyState _state = EnemyState.Idle;
+    private NavMeshAgent _agent;
+    //private Animator _animator;
+    private EnemyState _state;
+
+    public int Health;
+    public int MaxHealth = 50;
+
+    private Transform _target;          //플레이어
+    Vector3 _destination;
+    public float FindDistance = 7f;     //감지 거리
+    public float AttackDistance = 3f;   //공격 거리
+
+    public float PatrolTime = 3f;
+    private float _patrolTimer = 0f;
+    public float PatrolRadius = 30f;
+
+    private void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        //_animator = GetComponent<Animator>();
+        _target = GameObject.FindGameObjectWithTag("Player").transform;
+        _destination = transform.position;
+        Init();
+    }
+    private void Init()
+    {
+        _state = EnemyState.Idle;
+        Health = MaxHealth;
+        _patrolTimer = 0;
+    }
+
     private void Update()
     {
         switch (_state)
@@ -37,7 +68,17 @@ public class Enemy : MonoBehaviour
     }
     public void Idle()
     {
-
+        _patrolTimer += Time.deltaTime;
+        if (_patrolTimer > PatrolTime)
+        {
+            Debug.Log("Enemy: Idle -> Patrol");
+            _state = EnemyState.Patrol;
+            _patrolTimer = 0;
+        }
+        if (Vector3.Distance(_target.position, transform.position) <= FindDistance)
+        {
+            _state = EnemyState.Trace;
+        }
     }
     public void Patrol()
     {
