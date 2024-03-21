53fb08f6cace8f5f3f541483621cc267f5b4bd55
badabab <ky040723@gmail.com>
2024-03-21 17:49:34 +0900

    [김예은] 씬 수정

--- Assets/02. Scipts/UI/EndingScene.cs
+++ Assets/02. Scipts/UI/EndingScene.cs
@@ -16,29 +16,27 @@ public class EndingScene : MonoBehaviour
     }
     private IEnumerator ChangeTextCoroutine()
     {
-        EndingText.text = string.Empty;
-        yield return new WaitForSeconds(1);
-        EndingText.text = "- Original Game -\n\nELDEN RING";
-        yield return new WaitForSeconds(TextTime);
-        EndingText.text = string.Empty;
-        yield return new WaitForSeconds(1);
-        EndingText.text = "- Production Team -\n\nÀÌ°Ô ¿Ö µÅÁö";
-        yield return new WaitForSeconds(TextTime);
-        EndingText.text = string.Empty;
-        yield return new WaitForSeconds(1);
-        EndingText.text = "- Team Members -\n\n°øÁö¼ö  ÀÌ¼º¹Î  ±è¿¹Àº";
-        yield return new WaitForSeconds(TextTime);
-        EndingText.text = string.Empty;
-        yield return new WaitForSeconds(1);
-        EndingText.text = "- Production Period -\n\n24.03.05 ~ 24.03.22";
-        yield return new WaitForSeconds(TextTime);
-        EndingText.text = string.Empty;
-        yield return new WaitForSeconds(1);
         EndingText.text = "Thanks For Playing";
         yield return new WaitForSeconds(TextTime);
         EndingText.text = string.Empty;
         yield return new WaitForSeconds(1);
-        EndingText.fontSize = 100;
+        EndingText.text = "--- Original Game ---\n\nELDEN RING";
+        yield return new WaitForSeconds(TextTime);
+        EndingText.text = string.Empty;
+        yield return new WaitForSeconds(1);
+        EndingText.text = "--- Production Team ---\n\nÀÌ°Ô ¿Ö µÅÁö";
+        yield return new WaitForSeconds(TextTime);
+        EndingText.text = string.Empty;
+        yield return new WaitForSeconds(1);
+        EndingText.text = "--- Team Members ---\n\n°øÁö¼ö  ÀÌ¼º¹Î  ±è¿¹Àº";
+        yield return new WaitForSeconds(TextTime);
+        EndingText.text = string.Empty;
+        yield return new WaitForSeconds(1);
+        EndingText.text = "--- Production Period ---\n\n24.03.05 ~ 24.03.22";
+        yield return new WaitForSeconds(TextTime);
+        EndingText.text = string.Empty;
+        yield return new WaitForSeconds(1);
+        EndingText.fontSize = 80;
         EndingText.text = "- The End -";
     }
 }
